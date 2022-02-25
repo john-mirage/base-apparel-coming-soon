d33e192dd2fedc93e24c9a8947b63812046923eb
chaosdynamix <chaosdynamix@posteo.net>
2022-02-25 08:31:26 +0100

    feat(tailwind): add various configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,12 +11,14 @@ module.exports = {
       md: '768px',
       lg: '992px',
       xl: '1200px',
+      '2xl': '1400px',
     },
     colors: {
       'transparent': 'transparent',
       'desaturated-red': 'hsl(0, 36%, 70%)',
       'soft-red': 'hsl(0, 93%, 68%)',
       'dark-grayish-red': 'hsl(0, 6%, 24%)',
+      'white': 'hsl(0, 0%, 255%)',
     },
     extend: {
       fontFamily: {
@@ -24,6 +26,11 @@ module.exports = {
           'Josefin Sans',
           ...defaultTheme.fontFamily.sans,
         ]
+      },
+      backgroundImage: {
+        'hero-mobile': "url('/src/assets/images/hero-mobile.jpg')",
+        'hero-desktop': "url('/src/assets/images/hero-desktop.jpg')",
+        'pattern-desktop': "url('/src/assets/images/bg-pattern-desktop.svg')",
       },
     },
   },
